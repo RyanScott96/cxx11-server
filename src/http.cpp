26c38c19c0c93795009e5a86053e5656cb06045d
Ryan Scott <rscott@srspharmacy.com>
2024-07-11 14:31:58 -0400

    fix bug with how http class handles headers

--- src/http.cpp
+++ src/http.cpp
@@ -96,7 +96,7 @@ std::string http::Request::getHeader(std::string key) {
 };
 
 void http::Request::setHeader(std::string key, std::string value) {
-  this->headers.insert(std::make_pair(key, value));
+  this->headers[key] = value;
 };
 
 size_t http::Request::serialize(std::unique_ptr<unsigned char[]>& out) {
@@ -105,7 +105,7 @@ size_t http::Request::serialize(std::unique_ptr<unsigned char[]>& out) {
                              + http::version_as_string(this->getVersion()) + "\r\n";
   std::string headers = "";
   for (auto header : this->getHeaders()) {
-    headers += (header.first + ": " + header.second + "\r\n");
+    headers += (header.first + ":" + header.second + "\r\n");
   }
 
   std::string text = request_line + headers + "\r\n";
@@ -202,7 +202,7 @@ std::string http::Response::getHeader(std::string key) {
 };
 
 void http::Response::setHeader(std::string key, std::string value) {
-  this->headers.insert(std::make_pair(key, value));
+  this->headers[key] = value;
 };
 
  std::unique_ptr<unsigned char[]> http::Response::getData() {
@@ -224,7 +224,7 @@ size_t  http::Response::serialize(std::unique_ptr<unsigned char[]>& out) {
 
   head += version + " " + code + " " + statusText + "\r\n";
   for (auto header : this->headers) {
-    head += header.first + " : " + header.second + "\r\n";
+    head += header.first + ":" + header.second + "\r\n";
   }
   head += "\r\n";
 
@@ -239,7 +239,6 @@ std::map<std::string, std::string> http::Response::getDefaultReponseHeaders() {
   return {
       std::make_pair("Content-Type", "text/html; charset=UTF-8"),
       std::make_pair("Content-Encoding", "UTF-8"),
-      std::make_pair("Accept", "*/*"),
     };
 };
 
